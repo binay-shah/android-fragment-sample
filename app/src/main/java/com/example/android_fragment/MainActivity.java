90f79f7b3a3b73399f253b3ec22fea8b58675fce
binay-shah <binay18.shah@gmail.com>
2020-03-22 22:33:22 +0545

    Step.03-Exercise-ViewPager

--- app/src/main/java/com/example/android_fragment/MainActivity.java
+++ app/src/main/java/com/example/android_fragment/MainActivity.java
@@ -10,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO (2) Find the view pager that will allow the user to swipe between fragments
+
+        // TODO (3) Create an adapter that knows which fragment should be shown on each page
+
+        // TODO (4) Set the adapter onto the view pager
+
     }
 }
