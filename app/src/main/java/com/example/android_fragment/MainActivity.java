a58d732b9c04f1f7e35926f0962159a99bf72217
binay-shah <binay18.shah@gmail.com>
2020-03-22 21:04:07 +0545

    Step.01-Exercise-CreateFragment

--- app/src/main/java/com/example/android_fragment/MainActivity.java
+++ app/src/main/java/com/example/android_fragment/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.android_fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // TODO (1) Create a fragment1
+
+    Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO (3) Create a fragment 1 instance and display it using the Fragment manager
+        // TODO (4) dynamically add and replace fragments
+
     }
 }
