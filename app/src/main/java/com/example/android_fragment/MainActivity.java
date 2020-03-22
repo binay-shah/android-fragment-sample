b1dc158c35c6a16a10616ffde34f7e49a32fce5c
binay-shah <binay18.shah@gmail.com>
2020-03-22 22:15:26 +0545

    Step.02-Solution-DynamicFragments

--- app/src/main/java/com/example/android_fragment/MainActivity.java
+++ app/src/main/java/com/example/android_fragment/MainActivity.java
@@ -6,26 +6,51 @@ import androidx.fragment.app.FragmentManager;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button replaceButton;
+    private Fragment fragment;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        replaceButton = findViewById(R.id.replace_button);
 
+        replaceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        FragmentManager fm = getSupportFragmentManager();
+                if(fragment instanceof FirstFragment) {
+                    fragment = new SecondFragment();
+                    loadFragments(fragment);
+                }else if(fragment instanceof SecondFragment){
+                    fragment = new FirstFragment();
+                    loadFragments(fragment);
+                }
+            }
+        });
 
-
-        Fragment firstFragment = new FirstFragment();
-
-        fm.beginTransaction()
-                .add(R.id.fragment_container, firstFragment)
-                .commit();
+        fragment = new FirstFragment();
+        loadFragments(fragment);
     }
 
     // TODO (3) Create a method to load fragments
+
+    public void loadFragments(Fragment fragment){
+
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit();
+    }
+
 }
