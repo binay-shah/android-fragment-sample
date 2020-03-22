0de99f2ea4fbddd582dae3fc4c6d77f9845d4c54
binay-shah <binay18.shah@gmail.com>
2020-03-22 21:04:07 +0545

    Step.01-Exercise-CreateFragment

---
+++ app/src/main/java/com/example/android_fragment/FirstFragment.java
@@ -0,0 +1,32 @@
+package com.example.android_fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstFragment extends Fragment {
+
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView =  inflater.inflate(R.layout.fragment_first, container, false);
+        return rootView;
+    }
+
+}
