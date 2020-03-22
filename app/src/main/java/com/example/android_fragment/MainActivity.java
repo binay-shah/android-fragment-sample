7b03580ad7241b45f15511553b5d6d07c0b99b9b
binay-shah <binay18.shah@gmail.com>
2020-03-22 22:05:31 +0545

    Step.02-Exercise-DynamicFragments

--- app/src/main/java/com/example/android_fragment/MainActivity.java
+++ app/src/main/java/com/example/android_fragment/MainActivity.java
@@ -10,16 +10,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a fragment1
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO (3) Create a fragment 1 instance and display it using the Fragment manager
+
 
         FragmentManager fm = getSupportFragmentManager();
 
@@ -27,17 +23,9 @@ public class MainActivity extends AppCompatActivity {
         Fragment firstFragment = new FirstFragment();
 
         fm.beginTransaction()
-                .add(R.id.fragment_container1, firstFragment)
+                .add(R.id.fragment_container, firstFragment)
                 .commit();
-
-
-
-        Fragment secondFragment = new SecondFragment();
-        fm.beginTransaction()
-                .add(R.id.fragment_container2, secondFragment)
-                .commit();
-
-
-
     }
+
+    // TODO (3) Create a method to load fragments
 }
